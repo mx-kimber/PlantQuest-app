3cc85d3a9b5f2624c9c4470085c57e6925890b62
Kimber Ogden <mx.kimber@gmail.com>
2023-05-30 11:38:56 -0500

    Created skeleton

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer>
+      <p>Capstone project - Actualize - 2023
+      <a href="https://github.com/mx-kimber" label="GitHub" alt="GitHub"/>
+      </p>
+    </footer>
+  )
+}
