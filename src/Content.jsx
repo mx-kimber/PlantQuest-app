f62e3e9ebcc4a5bb62faa6a63011f12c9c243bbb
Kimber Ogden <mx.kimber@gmail.com>
2023-06-07 10:56:58 -0500

    Handle move to collection functionality in Content, cleaned up indentations

--- src/Content.jsx
+++ src/Content.jsx
@@ -4,18 +4,17 @@ import { PlantsIndex } from "./PlantsIndex";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { Modal } from "./Modal";
-// import { PlantsNew } from "./PlantsNew";
 import { PlantsShow } from "./PlantsShow";
 import { SchedulesIndex } from "./SchedulesIndex";
 import { SchedulesShow } from "./SchedulesShow";
 import { SchedulesNew } from "./SchedulesNew";
 import { CollectedPlantsIndex } from "./CollectedPlantsIndex";
 import { CollectedPlantsNew } from "./CollectedPlantsNew";
-import { CollectedPlantsShow } from "./CollectedPlantsShow"
+import { CollectedPlantsShow } from "./CollectedPlantsShow";
 import { Routes, Route } from "react-router-dom";
 import { About } from "./About";
 
-export function Content() {
+export function Content(props) {
   const [plants, setPlants] = useState([]);
   const [isPlantsShowVisible, setIsPlantsShowVisible] = useState(false);
   const [currentPlant, setCurrentPlant] = useState({});
@@ -41,15 +40,6 @@ export function Content() {
       setPlants(response.data);
     });
   };
-
-  // const handleCreatePlant = (params, successCallback) => {
-  //   axios.post("http://localhost:3000/plants.json", params).then((response) => {
-  //     setPlants([...plants, response.data]);
-  //     successCallback();
-  //     closeModal();
-  //     refreshIndex();
-  //   });
-  // };
 
   const handleShowPlant = (plant) => {
     setIsPlantsShowVisible(true);
@@ -216,23 +206,40 @@ export function Content() {
     const confirmed = window.confirm("Are you sure you want to delete this collected plant?");
     if (confirmed) {
       axios
-        .delete(`http://localhost:3000/collected_plants/${collectedPlant.id}.json`, { params: { confirm: "true" } })
-        .then((response) => {
-          const { message } = response.data;
-          if (message === "Collected plant destroyed successfully") {
-            console.log("Collected plant deleted successfully");
-            setCollectedPlants(collectedPlants.filter((cp) => cp.id !== collectedPlant.id));
-            setIsCollectedPlantsShowVisible(false);
-            closeModal();
-            refreshIndex();
-          } else {
-            console.log("Deletion canceled");
-          }
-        })
-        .catch(() => {
-          console.log("Error occurred during deletion");
-        });
+      .delete(`http://localhost:3000/collected_plants/${collectedPlant.id}.json`, { params: { confirm: "true" } })
+      .then((response) => {
+        const { message } = response.data;
+        if (message === "Collected plant destroyed successfully") {
+          console.log("Collected plant deleted successfully");
+          setCollectedPlants(collectedPlants.filter((cp) => cp.id !== collectedPlant.id));
+          setIsCollectedPlantsShowVisible(false);
+          closeModal();
+          refreshIndex();
+        } else {
+          console.log("Deletion canceled");
+        }
+      })
+      .catch(() => {
+        console.log("Error occurred during deletion");
+      });
     }
+  };
+  
+  const handleMoveToCollection = (plantId, currentUser) => {
+    const params = {
+      plant_id: plantId,
+      user_id: currentUser,
+    };
+
+    axios
+    .post("http://localhost:3000/collected_plants.json", params)
+    .then(() => {
+      console.log("Plant moved to collection successfully");
+      window.location.href = "/collected_plants";
+    })
+    .catch((error) => {
+      console.error("Error moving plant to collection:", error);
+    });
   };
   
 
@@ -249,58 +256,86 @@ export function Content() {
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
 
-   {/* // PLANTS */}
-      <Route path="/plants" element={<PlantsIndex
-      plants={plants} 
-      onShowPlant={handleShowPlant}/>} />
+    {/* // PLANTS */}
+      <Route
+        path="/plants"
+        element={
+          <PlantsIndex
+            plants={plants}
+            onShowPlant={handleShowPlant}
+            onMoveToCollection={handleMoveToCollection}
+          />
+        }
+      />
+        {/* <Route path="/plants/new" element={<PlantsNew 
+        onCreatePlant={handleCreatePlant} />} /> */}
+        
+      {/* // COLLECTED PLANTS */}
 
-      {/* <Route path="/plants/new" element={<PlantsNew 
-      onCreatePlant={handleCreatePlant} />} /> */}
+      <Route
+        path="/collected_plants"
+        element={<CollectedPlantsIndex
+          collectedPlants={collectedPlants}
+          onShowCollectedPlant={handleShowCollectedPlant}
+          onDestroyCollectedPlant={handleDestroyCollectedPlant}/>
+        }
+      />
+
+      <Route
+        path="/collected_plants/new"
+        element={<CollectedPlantsNew 
+          onCreateCollectedPlant={handleCreateCollectedPlant} />
+        }
+      />
+
+      {/* // SCHEDULES */}
+
+      <Route path ="/schedules" 
+        element={<SchedulesIndex 
+          schedules={schedules} 
+          onShowSchedule={handleShowSchedule} 
+          onUpdateSchedule={handleUpdateSchedule} 
+          onDestroySchedule={handleDestroySchedule} />
+        } 
+      />
+        
+      <Route path="/schedules/new" 
+        element={<SchedulesNew 
+          onCreateSchedule={handleCreateSchedule} />
+        } 
+      />
+    </Routes>
       
-    {/* // COLLECTED PLANTS */}
-      <Route path="/collected_plants" element={<CollectedPlantsIndex
-      collectedPlants={collectedPlants}
-      onShowCollectedPlant={handleShowCollectedPlant}
-      onDestroyCollectedPlant={handleDestroyCollectedPlant}/>} />
-
-      <Route path="/collected_plants/new" element={<CollectedPlantsNew 
-      onCreateCollectedPlant={handleCreateCollectedPlant} />} />
-
-    {/* // SCHEDULES */}
-      <Route path ="/schedules" element={<SchedulesIndex schedules={schedules} 
-      onShowSchedule={handleShowSchedule} 
-      onUpdateSchedule={handleUpdateSchedule} 
-      onDestroySchedule={handleDestroySchedule} />} />
-      
-      <Route path="/schedules/new" element={<SchedulesNew 
-      onCreateSchedule={handleCreateSchedule} />} />
-
-      </Routes>
-      
-
   {/* // MODALS  */}
+        <Modal show={isPlantsShowVisible} onClose={() => setIsPlantsShowVisible(false)}>
+        {currentPlant && (
+          <PlantsShow
+            plant={currentPlant}
+            // onUpdatePlant={handleUpdatePlant}
+            // onDestroyPlant={() => {
+            //   handleDestroyPlant(currentPlant);
+            //   setIsPlantsShowVisible(false);
+            //   refreshIndex();
+            // }}
+          />
+        )}
 
-      <Modal show={isPlantsShowVisible} onClose={() => setIsPlantsShowVisible(false)}>
-      {currentPlant && (
-        <PlantsShow
-          plant={currentPlant}
-          // onUpdatePlant={handleUpdatePlant}
-          // onDestroyPlant={() => {
-          //   handleDestroyPlant(currentPlant);
-          //   setIsPlantsShowVisible(false);
-          //   refreshIndex();
-          // }}
-        />
-      )}
-      <button onClick={() => {
-        handleIndexPlants();
-        setIsPlantsShowVisible(false);
-      }}>
-        All Plants
-      </button>
-    </Modal>
+        <button onClick={() => {
+          handleIndexPlants();
+          setIsPlantsShowVisible(false);
+        }}>
+          All Plants
+        </button>
 
-      <Modal show={isSchedulesShowVisible} onClose={() => setIsSchedulesShowVisible(false)}>
+        <button onClick={() => 
+          handleMoveToCollection(currentPlant.id, props.currentUser)}>
+          Move to collection
+        </button>
+      </Modal>
+
+
+      <Modal show={isSchedulesShowVisible} onClose={() => 
+        setIsSchedulesShowVisible(false)}>
         {currentSchedule && (
           <SchedulesShow
             schedule={currentSchedule}
@@ -316,7 +351,8 @@ export function Content() {
 
 
 
-      <Modal show={isCollectedPlantsShowVisible} onClose={() => setIsCollectedPlantsShowVisible(false)}>
+      <Modal show={isCollectedPlantsShowVisible} onClose={() => 
+        setIsCollectedPlantsShowVisible(false)}>
         {currentCollectedPlant && (
           <CollectedPlantsShow
             collectedPlant={currentCollectedPlant}
--- src/PlantsShow.jsx
+++ src/PlantsShow.jsx
@@ -1,44 +1,12 @@
 export function PlantsShow(props) {
-  
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const params = new FormData(event.target);
-    props.onUpdatePlant(props.plant.id, params) 
-    window.location.href = '/plants';
-  };
-
-  const handleDestroy = () => {
-    props.onDestroyPlant(props.plant);
-  };
 
   return (
-    <div>
+    <div id="plant-information">
       <h1>Plant information</h1>
       <p>Name: {props.plant.name}</p>
       <p>Description: {props.plant.description}</p>
       <p>Amount of Sun: {props.plant.sun_amount}</p>
-      <p>Days to water: {props.plant.days_to_water}</p>
-      <form onSubmit={handleSubmit}>
-        <div id="plant.edit-form">
-          <p>
-            Name: <input defaultValue={props.plant.name} name="name" type="text" required />
-          </p>
-        
-          <p>
-            Description: <input defaultValue={props.plant.description} name="description" type="text" required />
-          </p>
-        
-          <p>
-            Amount of Sun: <input defaultValue={props.plant.sun_amount} name="sun_amount" type="number" required />
-          </p>
-        
-          <p>
-            Days to water: <input defaultValue={props.plant.days_to_water} name="days_to_water" type="number" required />
-          </p>
-        </div>
-        <button type="submit">Update plant</button>
-      </form>
-      <button onClick={handleDestroy}>Destroy plant</button>
+      
     </div>
   );
 }
