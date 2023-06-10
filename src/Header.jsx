cebc6756993be14dfe6387140106063bba5e6dc8
Kimber Ogden <mx.kimber@gmail.com>
2023-06-09 23:50:40 -0500

    Show login/logout conditionlly

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,17 +2,26 @@ import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
+  let authenticationLinks;
+  if (localStorage.jwt === undefined) {
+    authenticationLinks = (
+      <>
+        <Link to="/login">Login</Link> | <Link to="/signup">Signup</Link>
+      </>
+    );
+  } else {
+    authenticationLinks = <LogoutLink />;
+  }
+
   return (
     <header>
       <nav>
-      <Link to="/about">About</Link>
-      <a href="/plants">Home</a>
-      <a href="/login">Login</a>
-      <a href="/Signup">Signup</a>
-      <LogoutLink />
-      <Link to="schedules">Schedules</Link>
-      <Link to="collected_plants">Collected Plants</Link>
+        <Link to="/about">About</Link>
+        <Link to="/plants">Home</Link>
+        <Link to="/schedules">Schedules</Link>
+        <Link to="/collected_plants">Collected Plants</Link>
+        {authenticationLinks}
       </nav>
     </header>
-  )
+  );
 }
