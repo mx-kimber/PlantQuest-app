e2778551f639f03d6f55777ec29f62955467c5cf
Kimber Ogden <mx.kimber@gmail.com>
2023-06-12 15:00:39 -0500

    Add bootstrap and webpack

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Header } from "./Header";
 import { Content } from "./Content";
 import { Footer } from "./Footer";
 import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable  */
+// Import our custom CSS
+import '../scss/styles.scss'
+import Nav from 'react-bootstrap/Nav';
+import * as bootstrap from 'bootstrap'
+import Alert from 'bootstrap/js/dist/alert'
+
+// // or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'
